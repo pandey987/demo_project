0af3b4b6ffdbabace89fb835cdc7636bd50e7824
Prakash <pandeyprakash9826@gmail.com>
2023-12-09 19:15:23 +0530

    second comment

--- script.js
+++ script.js
@@ -1,40 +1,129 @@
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': '7dd6587c98msha41a6d823203507p15d5f6jsnf320a9a3c959',
-			'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-		}
-	};
- 
-	const getwether = (city)=>{
-		cityname.innerHTML=city
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-		.then(response=> response.json())
-		.then((response)=>{
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7dd6587c98msha41a6d823203507p15d5f6jsnf320a9a3c959',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+let getwether = async (city) => {
+	cityname.innerHTML = city
+	let res = await axios.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then((response) => {
 			console.log(response)
 			// cloud_pct = response.cloud_pct
-			temp.innerHTML = response.temp
-			temp2.innerHTML = response.temp
-			feels_like.innerHTML = response.feels_like
-			humidity.innerHTML = response.humidity
-			humidity2.innerHTML = response.humidity
-			min_temp.innerHTML = response.min_temp
-			max_temp.innerHTML = response.max_temp
-			wind_speed.innerHTML = response.wind_speed
-			wind_speed2.innerHTML = response.wind_speed
-			wind_degrees.innerHTML = response.wind_degrees
-			sunrise.innerHTML = response.sunrise
-			sunset.innerHTML = response.sunset
-			
-			
+			temp.innerHTML = response.data.temp
+			temp2.innerHTML = response.data.temp
+			feels_like.innerHTML = response.data.feels_like
+			humidity.innerHTML = response.data.humidity
+			humidity2.innerHTML = response.data.humidity
+			min_temp.innerHTML = response.data.min_temp
+			max_temp.innerHTML = response.data.max_temp
+			wind_speed.innerHTML = response.data.wind_speed
+			wind_speed2.innerHTML = response.data.wind_speed
+			wind_degrees.innerHTML = response.data.wind_degrees
+			sunrise.innerHTML = response.data.sunrise
+			sunset.innerHTML = response.data.sunset
+
+
 		})
 		.catch(err => console.error(err));
-	}
-	
-submit.addEventListener("click",(e)=>{
+	// console.log(res);
+}
+
+submit.addEventListener("click", (e) => {
 	e.preventDefault();
-	getwether(city.value)
+	let val = document.querySelector("#city").value;
+	getwether(val)
 })
 getwether("delhi");
 
+
+let getwether2 = async (city) => {
+	let res = await axios.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then((response) => {
+			console.log(response)
+			cell11.innerHTML = response.data.cloud_pct
+			cell12.innerHTML = response.data.temp
+			cell13.innerHTML = response.data.feels_like
+			cell14.innerHTML = response.data.humidity
+			cell15.innerHTML = response.data.min_temp
+			cell16.innerHTML = response.data.max_temp
+			cell17.innerHTML = response.data.wind_speed
+			cell18.innerHTML = response.data.wind_degrees
+			cell19.innerHTML = response.data.sunrise
+			cell110.innerHTML = response.data.sunset
+
+
+		})
+		.catch(err => console.error(err));
+	// console.log(res);
+}
+getwether2("Shanghai");
+
+let getwether3 = async (city) => {
+	let res = await axios.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then((response) => {
+			console.log(response)
+			cell21.innerHTML = response.data.cloud_pct
+			cell22.innerHTML = response.data.temp
+			cell23.innerHTML = response.data.feels_like
+			cell24.innerHTML = response.data.humidity
+			cell25.innerHTML = response.data.min_temp
+			cell26.innerHTML = response.data.max_temp
+			cell27.innerHTML = response.data.wind_speed
+			cell28.innerHTML = response.data.wind_degrees
+			cell29.innerHTML = response.data.sunrise
+			cell210.innerHTML = response.data.sunset
+
+
+		})
+		.catch(err => console.error(err));
+	// console.log(res);
+}
+getwether3("Boston");
+
+let getwether4 = async (city) => {
+	let res = await axios.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then((response) => {
+			console.log(response)
+			cell31.innerHTML = response.data.cloud_pct
+			cell32.innerHTML = response.data.temp
+			cell33.innerHTML = response.data.feels_like
+			cell34.innerHTML = response.data.humidity
+			cell35.innerHTML = response.data.min_temp
+			cell36.innerHTML = response.data.max_temp
+			cell37.innerHTML = response.data.wind_speed
+			cell38.innerHTML = response.data.wind_degrees
+			cell39.innerHTML = response.data.sunrise
+			cell310.innerHTML = response.data.sunset
+
+
+		})
+		.catch(err => console.error(err));
+	// console.log(res);
+}
+getwether4("Allahabad");
+
+let getwether5 = async (city) => {
+	let res = await axios.get('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+		.then((response) => {
+			console.log(response)
+			cell41.innerHTML = response.data.cloud_pct
+			cell42.innerHTML = response.data.temp
+			cell43.innerHTML = response.data.feels_like
+			cell44.innerHTML = response.data.humidity
+			cell45.innerHTML = response.data.min_temp
+			cell46.innerHTML = response.data.max_temp
+			cell47.innerHTML = response.data.wind_speed
+			cell48.innerHTML = response.data.wind_degrees
+			cell49.innerHTML = response.data.sunrise
+			cell410.innerHTML = response.data.sunset
+
+
+		})
+		.catch(err => console.error(err));
+	// console.log(res);
+}
+getwether5("Tirupati");
 
